7ddebbb8cf5e967fd1400340c34856b171303641
ftreesmilo <58193105+ftreesmilo@users.noreply.github.com>
2022-05-22 15:57:34 -0700

    change to es modules

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 
 window.rpc.call('load', { video: 'video.mp4', loop: true }, false);
+
+const state = await window.rpc.call('state');
+state.views = (state.views || 0) + 1;
---
+++ webpack.config.babel.cjs
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./index.js",
+  devtool: "inline-source-map",
+  output: {
+    filename: "script.js",
+    path: path.join(__dirname, "dist"),
+  },
+  target: "web",
+  experiments: {
+    topLevelAwait: true,
+  },
+};
