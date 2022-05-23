81a9192cd834c3446cc50aa300701ac91a007bd9
ftreesmilo <58193105+ftreesmilo@users.noreply.github.com>
2022-05-22 18:01:52 -0700

    fix global rpc typing

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ window.rpc.call('load', { video: 'video.mp4', loop: true }, false);
 
 const state = await window.rpc.call('state');
 state.views = (state.views || 0) + 1;
+
+window.rpc.call('save', { state }, false);
