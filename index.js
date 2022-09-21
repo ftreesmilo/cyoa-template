15a6e0587248238a329c959f7a45ab31fa34f7e9
ftreesmilo <58193105+ftreesmilo@users.noreply.github.com>
2022-09-20 20:01:39 -0400

    add state demonstration

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ const state = await window.rpc.call('state');
 state.views = (state.views || 0) + 1;
 
 window.rpc.call('save', { state }, false);
+
+const choice = await window.rpc.call('prompt', {
+  content: `You've viewd this ${state.views} times.`,
+  choices: ['Ok']
+});
